14cb35295286467c9244d4a4d0949098ea8dd56e
Sumiran21 <sumiranmajgaonkar.com>
2024-04-02 00:21:35 -0700

    created two TestNG methods

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,19 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+
+	@Test(priority=1)
+	public void titleTest()
+
+	{
+		System.out.println("title test");
+	}
+	
+	@Test(priority=2)
+	public void applicationDescriptionTest()
+	{
+		System.out.println("app");
+	}
+}
